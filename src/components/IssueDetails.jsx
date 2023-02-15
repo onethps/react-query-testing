e8181fb4e6aacdca8247ee3567b9358dcefc2dce
evgenij <onethps@gmail.com>
2023-02-15 23:01:21 +0200

    ✨ feat(issue details):
    added basic details

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -1,4 +1,7 @@
+import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
+import { Comment } from "./Comment";
+import { IssueHeader } from "./IssueHeader";
 
 export const useIssueData = (issueNumber) => {
   return useQuery(["issues", issueNumber], async () => {
@@ -6,9 +9,41 @@ export const useIssueData = (issueNumber) => {
   });
 };
 
+function useIssueComments(issueNumber) {
+  return useQuery(["issues", issueNumber, "comments"], () => {
+    return fetch(`/api/issues/${issueNumber}/comments`).then((res) =>
+      res.json()
+    );
+  });
+}
+
 export default function IssueDetails() {
   const { number } = useParams();
   const issueQuery = useIssueData(number);
+  const commentsQuery = useIssueComments(number);
 
-  return <div>{issueQuery.isLoading ? <p>Loading...</p> : <div>22</div>}</div>;
+  return (
+    <div className="issue-details">
+      {issueQuery.isLoading ? (
+        <p>Loading issue...</p>
+      ) : (
+        <>
+          <IssueHeader {...issueQuery.data} />
+
+          <main>
+            <section>
+              {commentsQuery.isLoading ? (
+                <p>Loading...</p>
+              ) : (
+                commentsQuery.data?.map((comment) => (
+                  <Comment key={comment.id} {...comment} />
+                ))
+              )}
+            </section>
+            <aside></aside>
+          </main>
+        </>
+      )}
+    </div>
+  );
 }
